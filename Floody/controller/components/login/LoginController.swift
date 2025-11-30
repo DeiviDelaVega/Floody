e8fc22126551d27559184fc55659cb50cca053ff
Allinzon <allinzonhuaicha@gmail.com>
2025-11-30 18:10:32 -0500

    LoginDesing

---
+++ Floody/controller/components/login/LoginController.swift
@@ -0,0 +1,19 @@
+
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    @IBOutlet weak var txtCorreo: UITextField!
+    
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+ 
+    @IBAction func btnLogin(_ sender: UIButton) {
+    }
+    
+}
