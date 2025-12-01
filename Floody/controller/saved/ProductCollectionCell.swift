d1bab6d89fa9afa273dc94b377654c81ebee7ab5
Deivi De la Vega <deividelavegab@gmail.com>
2025-12-01 01:04:23 -0500

    SavedDesign

---
+++ Floody/controller/saved/ProductCollectionCell.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class ProductCollectionCell: UICollectionViewCell {
+    
+    @IBOutlet weak var ivProduct: UIImageView!
+    @IBOutlet weak var lblNameProduct: UILabel!
+    @IBOutlet weak var lblOriginProduct: UILabel!
+
+    
+    @IBOutlet weak var btnSaved: UIButton!
+    
+}
